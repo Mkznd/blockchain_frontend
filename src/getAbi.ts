e90ab8102cc5139e4e9946cc265f64e370d09b3c
Mkznd <yzundai1@gmail.com>
2025-01-29 20:28:00 +0100

    Add coverage to the tests & made abi a file

--- src/getAbi.ts
+++ src/getAbi.ts
@@ -1,5 +1,5 @@
+import contract from './contract.json';
+
 export default async function getAbi() {
-    const response = await fetch(import.meta.env.VITE_ABI_URL+"/Crowdfunding.sol/Crowdfunding.json");
-    const data = await response.json();
-    return data.abi;
+    return Promise.resolve(contract.abi);
 }
